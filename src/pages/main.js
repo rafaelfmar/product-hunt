cfe5a7fb4dee1f675f03fdb64e2176d735d986c0
marquesr1 <rafael.fmarques@outlook.com>
2019-09-08 14:14:23 -0300

    Getting data from API

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+import api from '../services/api';
+
 export default class main extends Component {
     static navigationOptions = {
         title: 'JSHunt'
+    };
+
+    state = {
+        docs: []
+    };
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+
+        const { docs } = response.data;
+
+        this.setState({ docs });
     };
 
     render() {
